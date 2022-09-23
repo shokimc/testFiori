70d7438556b08434c0b309952fafbe1fd25e7089
shokimc <shokimc@gmail.com>
2022-09-23 09:14:21 +0000

    Esto es un cambio2

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -9,7 +9,7 @@ sap.ui.define([
 
         return Controller.extend("project1.controller.View1", {
             onInit: function () {
-
+                console.log("Cambio");
             }
         });
     });
